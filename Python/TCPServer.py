a6f4eaae70f32d1ad82c69674bb2f7b6d7bd4d9d
Chun <zhengc42@gmail.com>
2019-08-20 15:41:58 -0400

    haven't touched this in years

--- Python/TCPServer.py
+++ Python/TCPServer.py
@@ -1,44 +1,26 @@
 #!/usr/local/bin/python
-"""
-Author: Chun Zheng 
-Language: Python 3.4.1
-Class: Networking 4450
-"""
 
-from socket import * 
-"""I imported multiprocessing and hope to use the Proces 
- so that i may just put it in a subprocess
- this took me sooooooooooooooo long Dr. Norris.  
-"""
+from click import echo
+from socket import *
 from multiprocessing import Process
 import time
 
-serverPort = 15020 
-serverSocket = socket(AF_INET, SOCK_STREAM)
+server_port = 15020
+server_socket = socket(AF_INET, SOCK_STREAM)
 
-serverSocket.bind(('',serverPort))
+server_socket.bind(('',server_port))
 
-serverSocket.listen(1)
-print("The Server is ready to recieve")
-#i put all the entire server call or execution in a 
-#function so i can thread it in a queue
+server_socket.listen(1)
+echo("The Server is ready to recieve")
 
-def connection(connectionSocket):
-    sentence = ''
-"""this while loop will keep looking for the 'quit' keyword so that
- it can close the socket after
-"""
-    while sentence != 'quit': 
-        sentence = connectionSocket.recv(1024)
-        capitalizedSentence = sentence.upper()
-        connectionSocket.send(capitalizedSentence)
-#socket closes when while loop is done. 
+def connection(connection_socket):
+    recieved_data = ''
+    while  recieved_data != 'quit':
+        received_data = connection_socket.recv(1024)
+        connection_socket.send(recieved_data.upper())
     connectionSocket.close()
-#this calls the connection function
-""" I have this while true so that i can multiThread
-and push the process to a sub process 
-"""
-while True: 
+
+while True:
     connectionSocket, addr = serverSocket.accept()
     p = Process(target=connection, args=(connectionSocket,))
     p.start()
